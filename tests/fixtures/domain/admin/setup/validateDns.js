6a642eac13056f7a4185c007c75a23af4b57e003
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2022-01-17 21:04:38 +0000

    Fix local DNS validation expression

--- tests/fixtures/domain/admin/setup/validateDns.js
+++ tests/fixtures/domain/admin/setup/validateDns.js
@@ -3,7 +3,7 @@ export default async function (state, settings, remote) {
   state.set(state.PROCESSING)
   if (
     settings.dns.domain !== ''
-    && isNaN(parseInt(settings.dns.accountId))
+    && !isNaN(parseInt(settings.dns.accountId))
     && settings.dns.accessToken !== ''
   ) {
     // Request remote validation.
@@ -16,6 +16,6 @@ export default async function (state, settings, remote) {
     state.set(state.OK)
   } else {
     // Preliminary client-side checks failed.
-    state.set(state.NOT_OK)
+    state.set(state.NOT_OK, { error: 'Failed local validations; please check DNS settings.'})
   }
 }
