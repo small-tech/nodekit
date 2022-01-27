a22bdc299bfb1569c95c61fcec41acd84ef21d2a
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2022-01-27 13:30:01 +0000

    Remove unused import

--- bin/nodekit.js
+++ bin/nodekit.js
@@ -3,8 +3,6 @@ import sade from 'sade'
 import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
-
 import Files from '../lib/Files.js'
 
 import packageInfo from '../package.json'
