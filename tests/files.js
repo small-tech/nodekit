f87e1e32d005d5ad59c65a2fecbeaff07cbbd83e
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2022-03-22 14:58:48 +0000

    Separate production/development tests for Files

--- tests/files.js
+++ tests/files.js
@@ -7,13 +7,61 @@ const fixturesPath = path.join(__dirname, 'fixtures')
 
 import Files from '../lib/Files.js'
 
-test('initialisation', async t => {
+function sortResults(filesByExtension, expectedFilesByExtension) {
+  // Chokidar’s file system scan does not always return the files in the same order.
+  // So we perform a sort on the actual and expected files before comparing them.
+  Object.keys(filesByExtension).forEach(extension => {
+    filesByExtension[extension].sort()
+  })
+  Object.keys(expectedFilesByExtension).forEach(extension => {
+    expectedFilesByExtension[extension].sort()
+  })
+  return { filesByExtension, expectedFilesByExtension }
+}
+
+test('production', async t => {
+  process.env.PRODUCTION = true
+
   const domainProjectPath = path.join(fixturesPath, 'files')
   const files = new Files(domainProjectPath)
 
-  const filesByExtension = await files.initialise()
+  const unsortedFilesByExtension = await files.initialise()
 
-  const expectedFilesByExtension = {
+  // During production, we only care about the files that we serve.
+  const unsortedExpectedFilesByExtension = {
+    page: [
+      "/home/aral/Projects/small-web/nodekit/app/tests/fixtures/files/admin/index.page",
+      "/home/aral/Projects/small-web/nodekit/app/tests/fixtures/files/index.page",
+      "/home/aral/Projects/small-web/nodekit/app/tests/fixtures/files/manage_[domain].page"
+    ],
+    socket: [
+      "/home/aral/Projects/small-web/nodekit/app/tests/fixtures/files/admin/index_[password].socket",
+      "/home/aral/Projects/small-web/nodekit/app/tests/fixtures/files/index.socket",
+      "/home/aral/Projects/small-web/nodekit/app/tests/fixtures/files/manage_[token]_[domain].socket"
+    ],
+    get: [
+      "/home/aral/Projects/small-web/nodekit/app/tests/fixtures/files/domain/available_[domain].get",
+      "/home/aral/Projects/small-web/nodekit/app/tests/fixtures/files/domain/ready_[domain].get",
+      "/home/aral/Projects/small-web/nodekit/app/tests/fixtures/files/private_[token]_[domain].get"
+    ]
+  }
+
+  const { filesByExtension, expectedFilesByExtension } = sortResults(unsortedFilesByExtension, unsortedExpectedFilesByExtension)
+  t.equals(JSON.stringify(filesByExtension), JSON.stringify(expectedFilesByExtension), 'files grouped by extension as expected (production)')
+
+  delete(process.env.PRODUCTION)
+})
+
+test('development', async t => {
+  delete(process.env.PRODUCTION)
+
+  const domainProjectPath = path.join(fixturesPath, 'files')
+  const files = new Files(domainProjectPath)
+  let unsortedFilesByExtension = await files.initialise()
+
+  // During development, we also care about the dependencies of files that we serve so we can
+  // implement live reload and CSS injection.
+  let unsortedExpectedFilesByExtension = {
     page: [
       '/home/aral/Projects/small-web/nodekit/app/tests/fixtures/files/index.page',
       '/home/aral/Projects/small-web/nodekit/app/tests/fixtures/files/manage_[domain].page',
@@ -46,16 +94,8 @@ test('initialisation', async t => {
     ]
   }
 
-  // Note: Chokidar’s file system scan does not always return the files in the same order.
-  // ===== So we perform a sort on the actual and expected files before comparing them.
-  Object.keys(expectedFilesByExtension).forEach(extension => {
-    expectedFilesByExtension[extension].sort()
-  })
-  Object.keys(filesByExtension).forEach(extension => {
-    filesByExtension[extension].sort()
-  })
-
-  t.equals(JSON.stringify(filesByExtension), JSON.stringify(expectedFilesByExtension), 'files grouped by extension as expected')
+  const { filesByExtension, expectedFilesByExtension } = sortResults(unsortedFilesByExtension, unsortedExpectedFilesByExtension)
+  t.equals(JSON.stringify(filesByExtension), JSON.stringify(expectedFilesByExtension), 'files grouped by extension as expected (development)')
 
   files.close()
 })
