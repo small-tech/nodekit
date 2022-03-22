55f1ddce3c7ab03225ce2647ca5e06db1b9d85b9
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2022-03-22 10:57:35 +0000

    Convert CLI tests to tape format

--- tests/cli.js
+++ tests/cli.js
@@ -1,42 +1,56 @@
-import { suite } from 'uvu'
-import * as assert from 'uvu/assert'
+import { test } from './helpers'
+
 import CLI from '../lib/cli/index.js'
 import serveCommand from '../lib/cli/commands/serve.js'
 import enableCommand from '../lib/cli/commands/enable.js'
 
-import { standardOutput } from './helpers'
-
 import packageInfo from '../package.json'
-
-const test = suite('CLI')
 
 let commands = (new CLI()).commands
 
-test('default', () => {
-  assert.equal(commands.default, 'serve', 'Default command is serve.')
+test('default', t => {
+  t.equals(commands.default, 'serve', 'Default command is serve.')
+  t.end()
 })
 
-test('version', () => {
-  assert.equal(commands.ver, packageInfo.version, 'CLI version matches package.json version.')
+test('version', t => {
+  t.plan(3)
+  t.equals(commands.ver, packageInfo.version, 'CLI version matches package.json version.')
 
-  standardOutput.capture()
+  // Monkey patch process.exit so version() doesn’t actually exit.
+  const originalProcessExit = process.exit
+
+  // Monkey patch console.log() so we can check the version being reported.
+  const originalConsoleLog = console.log
+
+  console.log = message => {
+    // We’re only interested in the first message. Revert console.log().
+    console.log = originalConsoleLog
+    t.equals(message, `NodeKit version ${packageInfo.version}`)
+  }
+
+  process.exit = code => {
+    t.equals(code, 0, 'process.exit() is called with success code')
+  }
+
   commands.parse([null, null, '--version'])
-  standardOutput.stopCapturing()
 
-  assert.equal(standardOutput.capturedContents.trim(), `nodekit, ${packageInfo.version}`, 'Version command output is as expected.')
+  // Revert process.exit().
+  process.exit = originalProcessExit
 })
 
-test('command handlers exist', () => {
-  assert.equal(commands.tree.serve.handler, serveCommand, 'Serve command handler exists.')
-  assert.equal(commands.tree.enable.handler, enableCommand, 'Enable command handler exists.')
+test('command handlers exist', t => {
+  t.equals(commands.tree.serve.handler, serveCommand, 'Serve command handler exists.')
+  t.equals(commands.tree.enable.handler, enableCommand, 'Enable command handler exists.')
+  t.end()
 })
 
-test('serve handler', () => {
+test('serve handler', t => {
   // TODO
+  t.end()
 })
 
-test('enable handler', () => {
+test('enable handler', t => {
   // TODO
+  t.end()
 })
-
-test.run()
