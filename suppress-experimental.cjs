02063233d323dc2218d0d97b835a6be7fe11b630
Aral Balkan <mail@ar.al>
2022-06-02 09:14:17 +0100

    Suppress experimental VM modules feature warning

--- suppress-experimental.cjs
+++ suppress-experimental.cjs
@@ -28,12 +28,16 @@ process.emit = function (name, data, ...args) {
     name === 'warning' &&
     typeof data === 'object' &&
     data.name === 'ExperimentalWarning' &&
-    (data.message.includes('--experimental-loader') ||
+    (
+      data.message.includes('--experimental-loader') ||
       data.message.includes('Custom ESM Loaders is an experimental feature') ||
 			data.message.includes('The Node.js specifier resolution flag is experimental') ||
-			data.message.includes('Importing JSON modules is an experimental feature'))
-  )
+			data.message.includes('Importing JSON modules is an experimental feature') ||
+      data.message.includes('VM Modules is an experimental feature')
+    )
+  ) {
     return false
+  }
 
   return originalEmit.apply(process, arguments)
 }
